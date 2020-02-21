1257d559b633820e3d8b3a70376e5da27816e326
emptyOne322 <se2@rep.earth>
2020-02-21 16:56:45 +0300

    Working on schedule with google api.

---
+++ src/authGoogleCalendar.js
@@ -0,0 +1,61 @@
+import fs from 'fs'
+import readline from 'readline'
+import { google } from 'googleapis'
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+const TOKEN_PATH = 'token.json';
+
+function authorize(credentials) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client);
+    oAuth2Client.setCredentials(JSON.parse(token));
+  });
+	return oAuth2Client
+}
+
+
+function getAccessToken(oAuth2Client) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+    });
+  });
+	
+}
+
+
+
+
+
+const authGoogleCalendar = async () => {
+	
+	const content = fs.readFileSync('credentials.json')
+	
+	const auth = authorize(JSON.parse(content))
+	
+	
+	return auth
+}
+
+
+export default authGoogleCalendar
