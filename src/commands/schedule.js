4335ec9cdcce0c30c1b49fa687032b913a0b920c
emptyOne322 <levaya.pochta347@gmail.com>
2020-03-18 15:08:09 +0300

    Added empty events list message.

--- src/commands/schedule.js
+++ src/commands/schedule.js
@@ -4,6 +4,39 @@ import { GOOGLE_API_KEY } from '../config'
 import formatDate from '../lib/formatDate'
 import { EMBED_COLOR } from './constants'
 
+export default {
+	name: 'schedule',
+	aliases: ['sch'],
+	description: 'Ближайшие стримы',
+	async execute(message, args) {
+		const events = await listEvents()
+		if(!events.length) {
+			const vpr = message.channel.guild.emojis.get('534814958186135553')
+			const vprAsString = vpr.toString()
+			message.channel.send(`Милорд <@${message.channel.guild.ownerID}>, народ требует расписания ${vprAsString}${vprAsString}${vprAsString}`);
+		}
+		else {
+			const eventsAsString = eventsToString(events)
+			
+			const embed = {
+				title: 'Ближашие стримы',
+				description: eventsAsString,
+				color: EMBED_COLOR,
+			}
+			message.channel.send(`<@${message.author.id}>`, {embed});
+		}
+		
+	},
+};
+
+const eventsToString = (events = []) => {
+	const eventsAsStringArray = events.map(e => {
+		const startTime = formatDate(e.start.dateTime)
+		return `**${startTime}** - ${e.summary}`
+	})
+	return eventsAsStringArray.join('\n')
+}
+
 const  listEvents = async () => {
 	let events
   const calendar = google.calendar({version: 'v3', auth: GOOGLE_API_KEY});
@@ -21,31 +54,6 @@ const  listEvents = async () => {
 	    events = res.data.items
 			resolve()
 	  })
-	}
-		
-	)
+	})
 	return events
 }
-
-export default {
-	name: 'schedule',
-	aliases: ['sch'],
-	description: 'Расписание стримов на неделю',
-	async execute(message, args) {
-		const events = await listEvents()
-		const eventsAsStringArray = events.map(e => {
-			const startTime = formatDate(e.start.dateTime)
-			return `**${startTime}** - ${e.summary}`
-		})
-		const eventsAsString = eventsAsStringArray.join('\n')
-		
-		const embed = {
-			color: EMBED_COLOR,
-			title: 'Ближашие стримы',
-			description: eventsAsString
-		}
-
-		
-		message.channel.send(`<@${message.author.id}>`, {embed});
-	},
-};
