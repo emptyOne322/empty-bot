19c4062c53a69c6be928e02c895b3f2c08657455
emptyOne322 <levaya.pochta347@gmail.com>
2020-07-30 13:43:09 +0300

    fix of invalid user in guildMemberRemove

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -3,10 +3,9 @@
 export default (member) => {
 	const { guild } = member
 	const memberTag = member.user.tag;
-	console.log(guild.emojis);
 	if(guild.systemChannel){
 		const vpr = guild.emojis.get('534814958186135553')
 		const vprAsString = vpr.toString()
-		guild.systemChannel.send(`<@${member.user.id}> покинул нас ${vprAsString}${vprAsString}${vprAsString}`);
+		guild.systemChannel.send(`${member.user.username} покинул нас ${vprAsString}${vprAsString}${vprAsString}`);
 	}
 }
