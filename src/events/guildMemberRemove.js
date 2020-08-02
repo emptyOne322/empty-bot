50ee8cca4a8baa4de992109d21caa46bcd010a1d
emptyOne322 <levaya.pochta347@gmail.com>
2020-08-02 03:17:06 +0300

    fix of username

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -3,6 +3,6 @@ export default (bot) => (member) => {
 	const memberTag = member.user.tag;
 	if(guild.systemChannel){
 		const vpr = bot.emojisDictionary['violet_pay_respect']
-		guild.systemChannel.send(`<@${member.user.id}> покинул нас ${vpr}${vpr}${vpr}`);
+		guild.systemChannel.send(`${member.user.username} покинул нас ${vpr}${vpr}${vpr}`);
 	}
 }
